4adf68d2a4a212a4d8474aef09f0c0588586a3d1
arahrojas00 <105313359+arahrojas00@users.noreply.github.com>
2025-03-05 22:09:52 -0600

    nodo_servicio.py

--- nodo_servicio.py
+++ nodo_servicio.py
@@ -3,6 +3,11 @@ import rclpy
 from rclpy.node import Node
 from cinematica_inversa_pkg.srv import DesiredPosition
 
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from cinematica_inversa_pkg.srv import DesiredPosition
+
 class ServicioPosicion(Node):
     def __init__(self):
         super().__init__('nodo_servicio')
@@ -11,11 +16,10 @@ class ServicioPosicion(Node):
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Esperando el servicio de cinemática inversa...')
 
-    def send_request(self, x, y, z):
+    def send_request(self, x, y):
         request = DesiredPosition.Request()
         request.x = x
         request.y = y
-        request.z = z
 
         future = self.cli.call_async(request)
         rclpy.spin_until_future_complete(self, future)
@@ -29,12 +33,11 @@ def main():
         try:
             x = float(input("Ingrese x: "))
             y = float(input("Ingrese y: "))
-            z = float(input("Ingrese z: "))
 
-            response = node.send_request(x, y, z)
+            response = node.send_request(x, y)
 
-            if response.x != -1 and response.y != -1 and response.z != -1:
-                node.get_logger().info(f"Posición alcanzable: ({response.x}, {response.y}, {response.z})")
+            if response.theta1 != -1 and response.theta2 != -1:
+                node.get_logger().info(f"Ángulos obtenidos: theta1={response.theta1}, theta2={response.theta2}")
             else:
                 node.get_logger().info("Posición NO alcanzable. Intente nuevamente.")
 
