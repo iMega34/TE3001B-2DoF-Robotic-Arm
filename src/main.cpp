55cb5e2864e6df3bae4343f52406a7a940806941
iMega34 <mega2207@outlook.com>
2025-03-09 19:54:20 -0600

    Use of Int16 for the node's datatype instead of Int16MultiArray

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-#include <std_msgs/msg/int16_multi_array.h>
+#include <std_msgs/msg/int16.h>
 
 #define ENCODER1_CA_PIN     23
 #define ENCODER1_CB_PIN     22
@@ -23,22 +23,24 @@
 
 #define LED_PIN             5
 
-#define ENCODER_RESOLUTION  580
+#define ENCODER1_RESOLUTION   493
+#define ENCODER2_RESOLUTION   493
 
 rcl_allocator_t allocator;
 rclc_executor_t executor;
 rclc_support_t support;
+rcl_timer_t timer;
 rcl_node_t node;
 
-rcl_timer_t timer;
-rcl_publisher_t encoders_pub;
-rcl_subscription_t control_law_sub;
+rcl_publisher_t encoder1_pub;
+rcl_publisher_t encoder2_pub;
+rcl_subscription_t control_law1_sub;
+rcl_subscription_t control_law2_sub;
 
-std_msgs__msg__Int16MultiArray encoders_msg;
-int16_t encoders_data[2] = {0, 0};
-
-std_msgs__msg__Int16MultiArray control_law_msg;
-int16_t control_law_data[2] = {0, 0};
+std_msgs__msg__Int16 encoder1_msg;
+std_msgs__msg__Int16 encoder2_msg;
+std_msgs__msg__Int16 motor1_msg;
+std_msgs__msg__Int16 motor2_msg;
 
 int16_t encoder1_count = 0;
 int16_t encoder2_count = 0;
@@ -53,14 +55,9 @@ void error_loop() {
   }
 }
 
-void subscription_callback(const void *msgin) {
-  const std_msgs__msg__Int16MultiArray *msg = (const std_msgs__msg__Int16MultiArray *)msgin;
-
-  control_law_data[0] = msg->data.data[0];
-  control_law_data[1] = msg->data.data[1];
-
-  motor1_pwm_signal = msg->data.data[0];
-  motor2_pwm_signal = msg->data.data[1];
+void lower_motor_callback(const void *msgin) {
+  const std_msgs__msg__Int16 *msg = (const std_msgs__msg__Int16 *)msgin;
+  motor1_pwm_signal = msg->data;
 
   if (motor1_pwm_signal < 0) {
     digitalWrite(MOTOR1_INA_PIN, HIGH);
@@ -70,6 +67,13 @@ void subscription_callback(const void *msgin) {
     digitalWrite(MOTOR1_INB_PIN, HIGH);
   }
 
+  analogWrite(MOTOR1_PWM_PIN, abs(motor1_pwm_signal));
+}
+
+void upper_motor_callback(const void *msgin) {
+  const std_msgs__msg__Int16 *msg = (const std_msgs__msg__Int16 *)msgin;
+  motor2_pwm_signal = msg->data;
+
   if (motor2_pwm_signal < 0) {
     digitalWrite(MOTOR2_INA_PIN, HIGH);
     digitalWrite(MOTOR2_INB_PIN, LOW);
@@ -78,38 +82,33 @@ void subscription_callback(const void *msgin) {
     digitalWrite(MOTOR2_INB_PIN, HIGH);
   }
 
-  analogWrite(MOTOR1_PWM_PIN, abs(motor1_pwm_signal));
   analogWrite(MOTOR2_PWM_PIN, abs(motor2_pwm_signal));
 }
 
 void timer_callback(rcl_timer_t* timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
+    encoder1_msg.data = (encoder1_count * 360 / ENCODER1_RESOLUTION) % 360;
+    encoder2_msg.data = (encoder2_count * 360 / ENCODER2_RESOLUTION) % 360;
 
-    encoders_data[0] = (encoder1_count * 360 / ENCODER_RESOLUTION) % 360;
-    encoders_data[1] = (encoder2_count * 360 / ENCODER_RESOLUTION) % 360;
-
-    encoders_msg.data.data = encoders_data;
-    encoders_msg.data.size = 2;
-    encoders_msg.data.capacity = 2;
-
-    rcl_publish(&encoders_pub, &encoders_msg, NULL);
+    rcl_publish(&encoder1_pub, &encoder1_msg, NULL);
+    rcl_publish(&encoder2_pub, &encoder2_msg, NULL);
   }
 }
 
 void encoder1_callback() {
   if (digitalRead(ENCODER1_CA_PIN) == digitalRead(ENCODER1_CB_PIN)) {
-    encoders_data[0] = encoder1_count++;
+    encoder1_count++;
   } else {
-    encoders_data[0] = encoder1_count--;
+    encoder1_count--;
   }
 }
 
 void encoder2_callback() {
   if (digitalRead(ENCODER2_CA_PIN) == digitalRead(ENCODER2_CB_PIN)) {
-    encoders_data[1] = encoder2_count++;
+    encoder2_count++;
   } else {
-    encoders_data[1] = encoder2_count--;
+    encoder2_count--;
   }
 }
 
@@ -149,45 +148,29 @@ void setup() {
     error_loop();
   }
 
-  encoders_msg.data.data = encoders_data;
-  encoders_msg.data.size = 2;
-  encoders_msg.data.capacity = 2;
-
   // Creates the publisher
-  if (rclc_publisher_init_default(&encoders_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16MultiArray), "encoders") != RCL_RET_OK ) {
-    error_loop();
-  }
+  rclc_publisher_init_default(&encoder1_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16), "lower_encoder");
+  rclc_publisher_init_default(&encoder2_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16), "upper_encoder");
 
-  control_law_msg.data.data = control_law_data;
-  control_law_msg.data.size = 2;
-  control_law_msg.data.capacity = 2;
-
-  if (rclc_subscription_init_default(&control_law_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16MultiArray), "control_law") != RCL_RET_OK) {
-    error_loop();
-  }
+  // Creates the subscriber
+  rclc_subscription_init_default(&control_law1_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16), "/lower_motor");
+  rclc_subscription_init_default(&control_law2_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16), "/upper_motor");
 
   // Creates the timer the publishes each 1000ms
-  if (rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(20), timer_callback) != RCL_RET_OK) {
-    error_loop();
-  }
+  rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(50), timer_callback);
 
   // Creates the executor to handle the publisher and subscriber
-  if (rclc_executor_init(&executor, &support.context, 2, &allocator) != RCL_RET_OK) {
-    error_loop();
-  }
+  rclc_executor_init(&executor, &support.context, 4, &allocator);
 
   // Add the timer to the executor
-  if (rclc_executor_add_timer(&executor, &timer) != RCL_RET_OK) {
-      error_loop();
-  }
+  rclc_executor_add_timer(&executor, &timer);
 
   // Agregar el suscriptor al ejecutor
-  if (rclc_executor_add_subscription(&executor, &control_law_sub, &control_law_msg, &subscription_callback, ON_NEW_DATA) != RCL_RET_OK) {
-    error_loop();
-  }
+  rclc_executor_add_subscription(&executor, &control_law1_sub, &motor1_msg, &lower_motor_callback, ON_NEW_DATA);
+  rclc_executor_add_subscription(&executor, &control_law2_sub, &motor2_msg, &upper_motor_callback, ON_NEW_DATA);
 }
 
 void loop() {
   delay(100);
-  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(20));
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(50));
 }
